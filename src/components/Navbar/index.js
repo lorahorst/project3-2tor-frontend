c4f11c274ec58be3729d74598aa7a1be1260970e
Lora Horst <lora.horst@t-online.de>
2022-04-12 18:40:06 +0200

    Installed Icon Fonts & Adjusted Nav Bar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,13 +2,15 @@ import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 import { AuthContext } from "context";
 import { useContext } from "react";
+import { AiFillHome } from 'react-icons/ai';
+import { FaUserInjured } from 'react-icons/fa';
 
 export function Navbar() {
   const { logout, user } = useContext(AuthContext);
   return (
     <nav>
-      {user && <Link to="/">Home</Link>}
-      {!user && <Link to="/landing">Landing</Link>}
+      {user && <Link to="/">Home<AiFillHome/></Link>}
+      {!user && <Link to="/landing">Landing<FaUserInjured /></Link>}
       {user && <button onClick={logout}>Logout</button>}
     </nav>
   );
