28f970bf34662f60c1af1e6019b109f32fd30493
Lora Horst <lora.horst@t-online.de>
2022-04-12 19:27:24 +0200

    Added some Styling to App.js & App.module.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
-import styles from "./App.module.css";
+import "./App.module.css";
 import { Navbar } from "components";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Navbar />
       <Outlet />
       footer
