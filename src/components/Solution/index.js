baf3f48918e2eff2a8f2ae620856494fccea28d5
Lora Horst <lora.horst@t-online.de>
2022-04-23 09:37:21 +0200

    Created Basic Homework + Solution Component and exported them

---
+++ src/components/Solution/index.js
@@ -0,0 +1,6 @@
+
+export function Solution () {
+    return (
+        'Solution'
+    )
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { Navbar } from "./Navbar";
 export { Signup } from "./Signup";
 export { Login } from "./Login";
 export { PrivateRoute } from "./PrivateRoute";
+export { Homework } from "./Homework";
+export { Solution } from "./Solution";
