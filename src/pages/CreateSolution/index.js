850a519c83e345c3e8c07fef81c40043f42118ee
Lora Horst <lora.horst@t-online.de>
2022-04-23 09:31:09 +0200

    exported all pages

---
+++ src/pages/CreateSolution/index.js
@@ -0,0 +1,7 @@
+
+
+export function CreateSolution() {
+  return (
+  'hello'
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
 export { Home } from "./Home";
 export { Landing } from "./Landing";
 export { NotFound } from "./NotFound";
+export { ShowHomework } from "./ShowHomework";
+export { ShowSolutions } from "./ShowSolutions";
+export { CreateHomework } from "./CreateHomework";
+export { CreateSolution } from "./CreateSolution";
