1c567f47834d3dc510f67bcd0af20cd1c7d53d52
Lora Horst <lora.horst@t-online.de>
2022-04-23 09:28:08 +0200

    Added Pages "ShowHomework" + "ShowSolutions"

---
+++ src/pages/ShowHomework/index.js
@@ -0,0 +1,5 @@
+import styles from "./ShowHomework.module.css";
+
+export function ShowHomework() {
+  return <div>Not found</div>;
+}
---
+++ src/pages/ShowSolutions/index.js
@@ -0,0 +1,5 @@
+import styles from "./ShowSolutions.module.css";
+
+export function ShowSolutions() {
+  return <div>Not found</div>;
+}
