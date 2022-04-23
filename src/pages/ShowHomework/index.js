356ce62948cd4ea07efbf82098d7cb0bd2117ee5
Lora Horst <lora.horst@t-online.de>
2022-04-23 10:26:12 +0200

    Worked on "ShowHomework" Page

--- src/pages/ShowHomework/index.js
+++ src/pages/ShowHomework/index.js
@@ -1,5 +1,13 @@
 import styles from "./ShowHomework.module.css";
+import { Homework } from "../../components";
 
-export function ShowHomework() {
-  return <div>Not found</div>;
+export function ShowHomework({ homeworks, setHomeworks }) {
+  (
+    <div>
+      {homeworks.map((homework) => {
+        // we pass the id, the content and the setTweets function
+        return <Homework key={homework._id} id={homework.id} content={homework.content} setHomeworks={setHomeworks} homework={homework} />;
+      })}
+    </div>
+  );
 }
