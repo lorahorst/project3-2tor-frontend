586a62faf73e67b52f0c6d5f93cc80b88fc1e400
Lora Horst <lora.horst@t-online.de>
2022-05-03 18:24:53 +0200

    css Update

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,35 +1,13 @@
-import axios from "axios";
-import { useContext, useEffect, useState } from "react";
+import { Login, Signup } from "components";
 import { AuthContext } from "context";
-import { ShowHomework2 } from "components";
+import { useContext } from "react";
 
-export function ShowHw() {
+export function Landing() {
   const { user } = useContext(AuthContext);
-  const [homeworks, setHomeworks] = useState([]);
-
-  const getHomeworks = async () => {
-   
-    const url = `${process.env.REACT_APP_BACKEND_URL}/hw`;
-  
-    const config = {
-      headers: {
-        authorization: localStorage.getItem("token"),
-      },
-    };
-    // make the request
-    const result = await axios.get(url, config);
-    setHomeworks(result.data);
-  };
-
-  useEffect(() => {
-    getHomeworks();
-  }, []);
-
   return (
     <div>
-      <h1>AllHomework</h1>
-      <ShowHomework2 homeworks={homeworks} />
+      <Login />
+      <Signup />
     </div>
   );
 }
-
